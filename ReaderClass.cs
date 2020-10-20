0a830398c3a6a1272745c812ea8b86c7e306d340
Zbigniew Pytka <zbigniew.pytka-ext@leica-geosystems.com>
2020-10-20 06:38:53 +0200

    Review 1

---
+++ ReaderClass.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class GenricReaderClass
+    {
+        string path = @"C:\fake.txt";
+        IReader realReader;
+
+        public GenricReaderClass(bool realMode)
+        {
+            realReader = realMode ? (IReader)new FileReader() : new FakeReader();
+        }
+
+        Task<IEnumerable<string>> ReadAsync()
+        {
+            return realReader.ReadAsync(path);
+        }
+
+    }
+
+    // END CODE TO REVIEW
+
+    public interface IReader
+    {
+        Task<IEnumerable<string>> ReadAsync(string param);
+    }
+
+    internal class FileReader : IReader
+    {
+        public async Task<IEnumerable<string>> ReadAsync(string param)
+        {
+            return await File.ReadAllLinesAsync(param);
+        }
+    }
+
+    internal class FakeReader : IReader
+    {
+        public async Task<IEnumerable<string>> ReadAsync(string param)
+        {
+            return await File.ReadAllLinesAsync(param);
+        }
+    }
+}
